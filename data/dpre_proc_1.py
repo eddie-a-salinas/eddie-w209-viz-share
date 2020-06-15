98a08470c7b5d5b80850374b68dc7564dc473752
eddie salinas <eddieasalinas@gmail.com>
2020-06-15 18:42:10 -0500

    added House key

--- data/dpre_proc_1.py
+++ data/dpre_proc_1.py
@@ -27,13 +27,13 @@ for f in file_to_house:
 			ddict=dict()
 			for i in range(len(dpieces)):
 				ddict[col_names[i]]=dpieces[i]
-			ddict['house']=file_to_house[f]
+			ddict['House']=file_to_house[f]
 			json_objs.append(ddict)
 
 ###############################
 # to avoid repeating the strings for the keys a lot ust an array here, reduce file size and parsing time!
 #esalina@2018comp:~/Documents/UC_Berkeley_MIDS/W209_DataVisualization/git_repos/w209-su2020-cybersec/data$ cat cols.dat |grep -Po '\..*'|tr -d "."|sed -r "s/^.//"|sort|uniq|awk '{print "\"" $0 "\"" }'|tr "\n" ","|sed -#r "s/,$/\n/"|awk '{print "[" $0 "]"}'
-keys=["Congress number","Day","Month","Number Democrat Nays","Number Democrat Yeas","Number Missing Votes (not voting and not in Congress)","Number Nays","Number Northern Democrat Nays","Number Northern Democrat Yeas","Number Northern Republican Nays","Number Northern Republican Yeas","Number Republican Nays","Number Republican Yeas","Number Southern Democrat Nays","Number Southern Democrat Yeas (11 States of Confederacy plus KY and OK)","Number Southern Republican Nays","Number Southern Republican Yeas (11 States of Confederacy plus KY and OK)","Number Yeas","Roll Call number","Year"]
+keys=["Congress number","Day","House","Month","Number Democrat Nays","Number Democrat Yeas","Number Missing Votes (not voting and not in Congress)","Number Nays","Number Northern Democrat Nays","Number Northern Democrat Yeas","Number Northern Republican Nays","Number Northern Republican Yeas","Number Republican Nays","Number Republican Yeas","Number Southern Democrat Nays","Number Southern Democrat Yeas (11 States of Confederacy plus KY and OK)","Number Southern Republican Nays","Number Southern Republican Yeas (11 States of Confederacy plus KY and OK)","Number Yeas","Roll Call number","Year"]
 json_compact=dict()
 json_compact['keys']=keys
 json_compact_data=list()
